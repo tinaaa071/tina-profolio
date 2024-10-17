9dc35904911e7a138cb7af4f094763a8ef3543a1
Tina <tina.chen@u-ark.com>
2024-10-17 09:35:35 +0800

    feat: add plugin section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,10 @@
     <template #content>
       <div class="space-y-5 sm:space-y-8">
         <!-- Hero 區塊 -->
-        <div class="grid grid-flow-row-dense grid-cols-4 gap-2.5 sm:gap-4 cursor-default">
+        <div class="grid grid-cols-4 grid-flow-row-dense gap-2.5 cursor-default sm:gap-4">
           <!-- 第一格 -->
-          <div class="inline-flex flex-col justify-between col-span-4 gap-6 p-5 bg-white xs:gap-10 sm:p-8 sm:col-span-2 sm:rounded-3xl dark:bg-transparent dark:border dark:border-stone-50 text-stone-900 dark:text-white rounded-2xl">
-            <div class="flex items-center gap-6 text-xl font-bold xs:text-2xl sm:text-3xl lg:text-4xl">
+          <div class="inline-flex flex-col col-span-4 gap-6 justify-between p-5 bg-white rounded-2xl xs:gap-10 sm:p-8 sm:col-span-2 sm:rounded-3xl dark:bg-transparent dark:border dark:border-stone-50 text-stone-900 dark:text-white">
+            <div class="flex gap-6 items-center text-xl font-bold xs:text-2xl sm:text-3xl lg:text-4xl">
               <!-- 照片 -->
               <div class="hidden w-32 h-32 bg-center bg-cover rounded-full xl:block"
               style="background-image: url(https://images.unsplash.com/photo-1723920515274-ace3503adad6?q=80&w=2826&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
@@ -29,22 +29,22 @@
             </div>
           </div>
           <!-- 第二格 -->
-          <div class="col-span-2 overflow-hidden cursor-pointer sm:rounded-3xl lg:col-span-1 rounded-2xl"
+          <div class="overflow-hidden col-span-2 rounded-2xl cursor-pointer sm:rounded-3xl lg:col-span-1"
           @click="isModalOpen = true"
           >
-            <div class="relative w-full h-full ">
-              <div class="absolute bottom-0 z-10 ">
+            <div class="relative w-full h-full">
+              <div class="absolute bottom-0 z-10">
                 <ArrowButton link-to="" @click="isModalOpen = true" />
               </div>
-              <div class="z-0 w-full h-full transition-transform duration-500 ease-in-out transform bg-center bg-cover rounded-3xl hover:scale-110"
+              <div class="z-0 w-full h-full bg-center bg-cover rounded-3xl transition-transform duration-500 ease-in-out transform hover:scale-110"
               style="background-image: url(https://images.unsplash.com/photo-1723920515274-ace3503adad6?q=80&w=2826&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
               </div>
             </div>
           </div>
           <!-- 第三格 -->
-          <RouterLink to="/" class="hidden row-span-2 overflow-hidden sm:rounded-3xl lg:block rounded-2xl">
+          <RouterLink to="/" class="hidden overflow-hidden row-span-2 rounded-2xl sm:rounded-3xl lg:block">
             <div class="relative w-full h-full">
-              <div class="absolute bottom-0 z-10 ">
+              <div class="absolute bottom-0 z-10">
                 <ArrowButton link-to="/" />
               </div>
               <p class="absolute w-fit h-fit inset-0 z-10 flex items-center justify-center text-6xl font-bold tracking-[.4em] text-white text-outline mx-auto my-auto"
@@ -52,7 +52,7 @@
               >
               {{ $t('hero.item9') }}
               </p>
-              <div class="w-full h-full transition-transform duration-500 ease-in-out transform bg-center bg-cover -z-10 rounded-3xl hover:scale-110 brightness-75"
+              <div class="w-full h-full bg-center bg-cover rounded-3xl brightness-75 transition-transform duration-500 ease-in-out transform -z-10 hover:scale-110"
               style="background-image: url(https://images.unsplash.com/photo-1724271366025-5bbfc5fbdb70?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
               </div>
             </div>
@@ -63,12 +63,12 @@
           </div>
           <!-- 第五格 -->
           <RouterLink to="/" class="hidden lg:block">
-            <div class="inline-flex items-center justify-center w-full h-full transition-colors duration-200 ease-in-out bg-blue-200 aspect-square sm:rounded-3xl rounded-2xl hover:bg-blue-200/75 group">
+            <div class="inline-flex justify-center items-center w-full h-full bg-blue-200 rounded-2xl transition-colors duration-200 ease-in-out aspect-square sm:rounded-3xl hover:bg-blue-200/75 group">
               <entypo-social-linkedin class="text-6xl text-white transition-all duration-200 ease-in-out dark:text-stone-900 group-hover:text-7xl" />
             </div>
           </RouterLink>
           <!-- 第六格 -->
-          <div class="col-span-2 p-5 bg-white md:p-8 sm:rounded-3xl dark:bg-transparent dark:border dark:border-stone-50 text-stone-900 dark:text-white lg:col-span-1 rounded-2xl">
+          <div class="col-span-2 p-5 bg-white rounded-2xl md:p-8 sm:rounded-3xl dark:bg-transparent dark:border dark:border-stone-50 text-stone-900 dark:text-white lg:col-span-1">
             <p class="mb-4 text-xs font-bold sm:text-sm md:text-lg">
               {{ $t('hero.item10') }}
             </p>
@@ -82,7 +82,56 @@
           </div>
         </div>
         <!-- Works 區塊 -->
-         <WorkCard />
+        <div class="flex flex-col gap-2.5 cursor-default sm:gap-4">
+          <!-- Works -->
+          <WorkCard />
+          <!-- 其他資源 -->
+          <div class="grid overflow-hidden grid-cols-1 gap-2.5 cursor-default lg:grid-cols-2 text-stone-900 dark:text-white h-fit dark:bg-transparent dark:border dark:border-white sm:gap-4">
+            <!-- Plugin 區塊 -->
+            <div class="p-5 bg-white rounded-2xl sm:p-8 dark:bg-transparent sm:rounded-3x">
+              <div class="flex justify-between mb-4 lg:mb-10 xs:mb-6">
+                <p class="tracking-[.25em] text-xs xs:text-base xl:text-lg font-bold ">
+                  {{ $t('work.core.item1') }}
+                </p>
+                <p class="text-lg font-extrabold xs:text-2xl sm:text-3xl lg:sm:text-4xl">
+                  Shape Mask
+                </p>
+              </div>
+              <RouterLink to="/">
+                <div class="overflow-hidden rounded-2xl sm:rounded-3xl">
+                  <img src="../assets/plugin-banner.png" alt="" class="transition-all duration-300 hover:scale-105">
+                </div>
+              </RouterLink>
+            </div>
+
+            <!-- Blog 區塊 -->
+            <div class="p-5 bg-white rounded-2xl sm:p-8 dark:bg-transparent sm:rounded-3xl">
+              <p class="tracking-[.25em] lg:mb-10 mb-4 xs:mb-6 text-xs xs:text-base xl:text-lg font-bold ">
+                {{ $t('work.core.item1') }}
+              </p>
+              <div class="space-y-3 text-lg font-extrabold lg:space-y-5 xs:text-2xl sm:text-3xl lg:sm:text-4xl text-stone-400 dark:text-stone-400">
+                <!-- Project Buttons -->
+                 <RouterLink to="">
+                  
+                 </RouterLink>
+                <button
+                  v-for="tab in [1, 2, 3, 4]"
+                  :key="tab"
+                  type="button"
+                  class="inline-flex w-full leading-normal text-left transition-colors duration-300 ease-in-out"
+                  :class="{
+                    'text-stone-900 dark:text-white': activeTab === tab,
+                    'focus:text-stone-900 hover:text-stone-900 dark:focus:text-white dark:hover:text-white': activeTab !== tab
+                  }"
+                  @click="selectTab(tab)"
+                >
+                  <p class="whitespace-nowrap">{{ `0${tab} -` }}&nbsp;</p>
+                  {{ $t(`project${tab}.core.title`) }}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </template>
   </Layout>
