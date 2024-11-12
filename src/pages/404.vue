535ca06172e308ea96d559d9af3bb44030386bf8
Tina <tina.chen@u-ark.com>
2024-11-12 16:04:19 +0800

    style: 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen text-gray-700 bg-gray-100">
+    <div class="flex flex-col justify-center items-center px-6 min-h-screen text-gray-700 bg-gray-100">
       <div class="text-center">
-        <h1 class="font-extrabold text-indigo-600 text-9xl">404</h1>
+        <h1 class="text-9xl font-extrabold text-indigo-600">404</h1>
         <p class="mt-4 text-2xl font-bold md:text-3xl">Oops! Page Not Found</p>
         <p class="mt-2 text-lg text-gray-500">The page you are looking for does not exist or an error occurred.</p>
-        <RouterLink to="/" class="inline-block px-6 py-3 mt-6 text-white transition duration-300 ease-in-out rounded-md shadow-md bg-stone-800 hover:bg-stone-300">
+        <RouterLink to="/" class="inline-block px-6 py-3 mt-6 text-white rounded-md shadow-md transition duration-300 ease-in-out bg-stone-800 hover:bg-stone-300">
           Go Back Home
         </RouterLink>
       </div>
