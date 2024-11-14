8ab9e76e0c341e42a09ace7633d1cd3aef42826e
Tina <tina.chen@u-ark.com>
2024-11-14 10:45:07 +0800

    test: lottie path

--- src/pages/others.vue
+++ src/pages/others.vue
@@ -72,7 +72,7 @@
             title: "Post 1",
             category: this.$t('other.item1'),
             // image: "https://images.unsplash.com/photo-1628768534904-cf74bc8b897d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            lottiePath: "../assets/aaa.json"
+            lottiePath: "../src/assets/aaa.json"
           },
         {
             id: 2,
